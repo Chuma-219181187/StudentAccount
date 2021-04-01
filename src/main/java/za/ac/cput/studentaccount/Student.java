9a4841e258083be9a3a485f798797ce810f43858
27630 <27630@LAPTOP-QQSQDDOI>
2021-04-01 02:08:08 +0200

    student java file pushed to 219181187

--- src/main/java/za/ac/cput/studentaccount/Student.java
+++ src/main/java/za/ac/cput/studentaccount/Student.java
@@ -11,12 +11,42 @@ package za.ac.cput.studentaccount;
  */
 public class Student {
    
-    
+     private String name, surname, department;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", surname=" + surname + ", department=" + department + '}';
+    }
    
-    
-    
-    
-    
-    
-    
+     public String displayStudentName1 (String a, String b ){
+        
+        return a + b;
+    }
+   
 }
+  
+   
