e47a297b70283fedaf9fd5c492bdeb61c5f6075b
Mogamad Githr <mogamadgithr@Mogamads-MacBook-Air.local>
2021-04-01 20:56:58 +0200

    Added a subject class

---
+++ src/main/java/za/ac/cput/studentaccount/Subject.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.studentaccount;
+
+/**
+ *
+ * @author mogamadgithr
+ */
+public class Subject {
+    
+    private String type, code, name;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" + "type=" + type + ", code=" + code + ", name=" + name + '}';
+    }
+    
+}
